dd9bd113ee9e33396d965ef40e5215e9b5efbc93
Reece Nunn <rmnunn5@gmail.com>
2020-01-29 16:07:37 -0700

    Added vardump to index.php

--- php/lib/index.php
+++ php/lib/index.php
@@ -11,4 +11,6 @@ $hash = password_hash("password", PASSWORD_ARGON2I, ["time_cost=>7"]);
 $newAuthor = new Author("cad80653-b18c-42db-869f-b93bf2ffffef", "12345678901234567890123456789012", "www.avatarurl.com",
 	"rnunn@dsf.edu", $hash, "rnunn4");
 
+var_dump($newAuthor);
+
 echo ($newAuthor->getAuthorEmail()), ($newAuthor->getAuthorActivationToken()), ($newAuthor->getAuthorAvatarUrl()), ($newAuthor->getAuthorEmail()), ($newAuthor->getAuthorHash()), ($newAuthor->getAuthorUserName());
